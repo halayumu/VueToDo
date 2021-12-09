f48eaf8488aa1586db4c42c076497bfff6d66f1f
halayumu <ookamiayumuwww@gmail.com>
2021-12-10 04:09:16 +0900

    入力情報の多数表示修正

--- main.js
+++ main.js
@@ -1,13 +1,35 @@
-new Vue({
-    el: "#app",
-    data: {
-        text: "",
-        todos: [],
+// new Vue({
+//     el: "#app",
+//     data: {
+//         text: "",
+//         todos: [],
+//     },
+
+//     methods: {
+//         addTexts: function() {
+//             this.todos.push({ name: this.text })
+//         }
+//     }
+// })
+
+const app = new Vue({
+    el: '#app',
+    data() {
+        return {
+            inputText: '', //テキストデータの代入
+            lists: [] //空配列
+        };
     },
 
     methods: {
-        addTexts: function() {
-            this.todos.push({ name: this.text })
+        onAdd() {
+            if (this.inputText) {
+                this.lists.push({ //配列代入
+                    name: this.inputText, //name変数にテキストデータ代入
+                    // isDone: false,
+                });
+                // this.inputText = '';
+            }
         }
     }
-})
+});
