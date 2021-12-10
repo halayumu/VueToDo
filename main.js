735181e786d1b9974ee5fd7754eb5b8eafff4c26
halayumu <ookamiayumuwww@gmail.com>
2021-12-11 03:12:21 +0900

    textの入力から一覧完了

--- main.js
+++ main.js
@@ -1,34 +1,16 @@
-// new Vue({
-//     el: "#app",
-//     data: {
-//         text: "",
-//         todos: [],
-//     },
-
-//     methods: {
-//         addTexts: function() {
-//             this.todos.push({ name: this.text })
-//         }
-//     }
-// })
-
-const app = new Vue({
-    el: '#app',
-    data() {
-        return {
-            inputText: '', //テキストデータの代入
-            lists: [] //空配列
-        };
+new Vue({
+    el: "#app",
+    data: {
+        text: "", //テキストデータの代入
+        lists: [] //空配列
     },
 
     methods: {
-        onAdd() {
-            if (this.inputText) {
-                this.lists.push({ //配列代入
-                    name: this.inputText, //name変数にテキストデータ代入
-                    // isDone: false,
+        addTexts: function() {
+            if (this.text) {
+                this.lists.push({ //空配列にテキストデータを代入
+                    name: this.text, //name変数にテキストデーター代入
                 });
-                // this.inputText = '';
             }
         }
     }
